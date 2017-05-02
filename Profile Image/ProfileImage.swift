65de796dc0c89b9209885157bfabfb6836ac0f5b
Tejas Ardeshna <Tejas.a@simform.in>
2017-05-02 18:11:55 +0530

    initial commit

---
+++ Profile Image/ProfileImage.swift
@@ -0,0 +1,79 @@
+//
+//  ProfileImage.swift
+//  Profile Image
+//
+//  Created by Tejas Ardeshna on 02/05/17.
+//  Copyright © 2017 Tejas Ardeshna. All rights reserved.
+//
+
+import UIKit
+@IBDesignable class ProfileImage:UIImageView {
+    
+    @IBInspectable var dashedBorder: Bool = false {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var isCircular: Bool = false {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var cornerRadious: CGFloat = 5 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layoutIfNeeded()
+        self.applyProperties()
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.setNeedsDisplay()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setNeedsDisplay()
+    }
+
+    override func layoutSubviews() {
+        self.applyProperties()
+    }
+    func applyProperties()
+    {
+        let shapeLayer:CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+        
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = borderColor.cgColor
+        shapeLayer.lineWidth = borderWidth
+        shapeLayer.lineJoin = kCALineJoinRound
+        if dashedBorder{
+            shapeLayer.lineDashPattern = [6,3]
+        }
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: isCircular ? CGFloat(self.frame.size.height / 2.0) :  cornerRadious).cgPath
+        self.layer.cornerRadius = isCircular ? CGFloat(self.frame.size.height / 2.0) :  cornerRadious
+        self.layer.masksToBounds = true
+        self.layer.addSublayer(shapeLayer)
+        //self.layer.insertSublayer(shapeLayer, at: 0)
+    }
+    
+}
---
+++ Profile Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile Image
+//
+//  Created by Tejas Ardeshna on 02/05/17.
+//  Copyright © 2017 Tejas Ardeshna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
